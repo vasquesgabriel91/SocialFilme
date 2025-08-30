cda4909919287d84ad050d2233f21e84d8c79094
vasquesgabriel91 <vasquesgabriel91@gmail.com>
2025-08-29 22:28:02 -0300

    fix: retornando a bio

--- src/usuario/UserModel.js
+++ src/usuario/UserModel.js
@@ -8,6 +8,8 @@ User.init({
     username: { type: DataTypes.STRING(50), allowNull: false, unique: true },
     email: { type: DataTypes.STRING(100), allowNull: false, unique: true },
     password: { type: DataTypes.STRING(100), allowNull: false },
+    bio: { type: DataTypes.STRING, allowNull: true },
+
 },{
     sequelize,
     modelName: "User",
